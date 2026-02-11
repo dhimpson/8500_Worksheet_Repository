86e7a21a1f480627504c83b651e5f7b8bff52d72
dhimpson <dorai@clemson.edu>
2026-02-11 14:06:34 -0500

    worksheet 3 loops work

--- notes.r
+++ notes.r
@@ -5,3 +5,24 @@ nrow(BostonWomenVoters)
 rows <- 10702
 rows > 100
 #notes from February 2 (zoom class)
+library(DigitalMethodsData)
+data(BostonWomenVoters)
+if (BostonWomenVoters$Occupation[4] == "Housewife") {
+    print ("this is a housewife")
+}
+i <- 1
+while (i <= nrow(BostonWomenVoters)) {
+    if (BostonWomenVoters$Occupation[4] == "Housewife")
+    print (i)
+i <- i +1
+}
+
+i <- i +1
+if (BostonWomenVoters$Occupation[4] == "Housewife") {
+  print("This woman is a housewife")
+} else if (BostonWomenVoters$Occupation[4] == "Stenographer") {
+  print("this woman is a stenographer")
+} else {
+  print("this woman is neither")
+}
+for (i in nrow(BostonWomenVoters [1]))
